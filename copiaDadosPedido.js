cf84a8bc9d71247d98ce3a86d9e452def15d699a
Tsudizera <106840606+Tsudizera@users.noreply.github.com>
2023-08-30 12:36:15 -0300

    suporte ao novo layout magalu
    alguns ajuste em data, b2w e amz

--- copiaDadosPedido.js
+++ copiaDadosPedido.js
@@ -1,30 +1,29 @@
 /*javascript:
-/* COPIA (DATA/PEDIDO/MKT/NOME/CPF[/MOD/SKU/QTD]) DA (MAGALU/B2W/ML/MLFULL(!)/AMZ/VIA(!)/SITE) */
-/* substituir 'querySelector' por '$' */
+/* copia dados pedidos (DATA/PEDIDO/MKT/NOME/CPF[/MOD/SKU/QTD]) DA (MAGALU/B2W/ML/MLFULL(!)/AMZ/VIA(!)/SITE) */
 /* (!) Parcial! Pendencias da VIA e MLFULL */
 
 console.clear();
-console.log("VERSÃO: 2023 FEV 08");
+console.log("VERSÃO: 2023-08-30");
 var $ = (q, parent = document) => parent.querySelector(q);
 var $$ = (q, parent = document) => [...parent.querySelectorAll(q)];
 
 
 function ctrl_C(pedido, mkt, nome, cpf, listaItens) {
   const agora = new Date();
-  const dia = ("0" + agora.getDate()).slice(-2) + "/" + ("0" + (agora.getMonth() + 1)).slice(-2) + "/" + agora.getFullYear();
+  const dia = agora.getFullYear() + '-' + ("0" + (agora.getMonth() + 1)).slice(-2) + '-' + ("0" + agora.getDate()).slice(-2);
   pedido = `=HIPERLINK("${window.location.href}";"${pedido}")`;
 
   const modelo = listaItens.reduce((acc, maluco) => [...acc, maluco.modelo], []).join("\n");
   const sku = listaItens.reduce((acc, maluco) => [...acc, maluco.sku.replace(/\D/g, "")], []).join("\n");
   const qtd = listaItens.reduce((acc, maluco) => [...acc, maluco.qtd.replace(/\D/g, "")], []).join("\n");
 
-  const el = document.createElement('textarea');
-  el.value = `${dia}\t${pedido}\t${mkt}\t${nome}\t${cpf}\t"${modelo}"\t"${sku}"\t"${qtd}"`;
-  console.log(el.value);
-  document.body.appendChild(el);
-  el.select();
+  const $textarea = document.createElement('textarea');
+  $textarea.value = `${dia}\t${pedido}\t${mkt}\t${nome}\t${cpf}\t"${modelo}"\t"${sku}"\t"${qtd}"`;
+  console.log($textarea.value);
+  document.body.appendChild($textarea);
+  $textarea.select();
   document.execCommand('copy');
-  el.remove();
+  $textarea.remove();
 }
 
 
@@ -34,18 +33,17 @@ try {
 
     case link.includes("integracommerce.com.br/Order"): {
       console.log("MGL");
-      const pedido = $$(".pl10 ,.valign-top")[0].innerText;
-      const nome = $$(".pl10 ,.valign-top")[14].innerText;
-      const cpf = $$(".pl10 ,.valign-top")[15].innerText;
+      const pedido = $('#IdOrder').value;
+      const nome = $('#CustomerPfName').value;
+      const cpf = $('#CustomerPfCpf').value;
 
-      const itens = $$("div.col-sm-6", $("div.row.pr15.pl15.mt15"));
+      const $$produtos = $$("div.row.pr15.pl15.mt15 div.col-sm-6");
       const listaItens = [];
-      for (const i of itens) {
-        const umaConstanteComNomeRuim = $$("td.pl15.valign-top", i);
+      for (const $1produto of $$produtos) {
         const obj = {
-          modelo: umaConstanteComNomeRuim[0].innerText,
-          sku: umaConstanteComNomeRuim[1].innerText,
-          qtd: umaConstanteComNomeRuim[3].innerText
+          modelo: $('[for="orderProduct_Name"]', $1produto).parentElement.nextElementSibling.innerText,
+          sku: $('[for="orderProduct_SkuId"]', $1produto).parentElement.nextElementSibling.innerText,
+          qtd: $('[for="orderProduct_Quantity"]', $1produto).parentElement.nextElementSibling.innerText,
         };
         listaItens.push(obj);
       }
@@ -57,8 +55,8 @@ try {
     case link.includes("americanasmarketplace.com.br/v3/pedidos/detalhes"): {
       console.log("B2W");
       const pedido = $(".timeline-delivery .p14").innerText;
-      const nome = $$('section[_ngcontent-c22] .card .col-md-12')[1].querySelector("span").innerText;
-      const cpf = $$('section[_ngcontent-c22] .card .col-md-4')[0].querySelector("span").innerText;
+      const nome = $("span", $$('section[_ngcontent-c22] .card .col-md-12')[1]).innerText;
+      const cpf = $('span', $$('section[_ngcontent-c22] .card .col-md-4')[0]).innerText;
 
       const item = $$("div.card")[3];
       const listaModelo = $$(".p14-bold", item);
@@ -82,9 +80,9 @@ try {
       const pedido = $$(".sc-text")[0].innerText.match(/#\d*/g)[0].replace(/#/g, "");
       const cpf = $("div.sc-buyer__content").innerText.split(" CPF ")[1];
       const nome = $("div.sc-buyer__content b").innerText;
-      
+
       const listaItens = [];
-      
+
       if ($(".sc-status__title-detail-container").innerHTML.includes("<svg")) {
         console.log("ML FULL");
         console.warn('ITENS PENDENTE! Não sei como fica o html quando compra mais que 1 item diferente. Se pá é igual ao ML normal.');
@@ -108,7 +106,7 @@ try {
       console.log("AMZ");
       const pedido = $("span[data-test-id=order-id-value]").innerText;
       const nome = $$("div[data-test-id=shipping-section-buyer-address] span")[0].innerText.slice(0, -1);
-      const cpf = $$("div.a-box")[2].querySelectorAll("span")[6].innerText;
+      const cpf = $$(`span`, $$("div.a-box")[2])[6].innerText;
       const itens = $$("div.a-row.a-spacing-large table.a-keyvalue>tbody>tr");
 
       const listaItens = [];
